54f0a21e11708b527634283eec2eb097e7e86b34
Tiago Marinho <leduck@outlook.com>
2019-05-21 21:16:51 -0300

    Delete qualityoption.js

--- qualityoption.js
+++
@@ -1,7 +0,0 @@
-class QualityOption extends Option {
-	constructor (name, defaultIndex = 0, description = "") {
-		let states = ["low", "medium", "high"]
-
-		super(name, states, defaultIndex, description)
-	}
-}
