88ad0dcba61670e817661b0c513277c03fe59ae1
Tiago Marinho <leduck@outlook.com>
2019-06-05 14:29:17 -0300

    Delete createdownloadablefile.js

--- js/createdownloadablefile.js
+++
@@ -1,6 +0,0 @@
-createDownloadableFile(fileName, content) {
-   let link = document.createElement("a");
-   link.download = fileName;
-   link.href = `data:application/octet-stream,${content}`;
-   return link;
- }
